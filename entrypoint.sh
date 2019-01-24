4d148ed65579e52dbe73b91bfdd2f0d2d18a2bc3
bonf <dan.bonifazi@gmail.com>
2019-01-24 13:26:55 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Entrypoint"
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
