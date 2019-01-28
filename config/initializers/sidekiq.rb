0d5835fda9bf233a543271b23872706c899d4129
bonf <dan.bonifazi@gmail.com>
2019-01-28 18:04:16 +0100

    saved

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
-sidekiq_config = { url: ENV['REDIS_URL'] }
+#sidekiq_config = { url: ENV['REDIS_URL'] }
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: sidekiq_config }
+  #config.redis = { url: sidekiq_config }
+  config.redis = { url: ENV['REDIS_URL']}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: sidekiq_config }
+  #config.redis = { url: sidekiq_config }
+  config.redis = { url: ENV['REDIS_URL']}
 end
